a8413539a3c34825139c68a668e10b9983a20dc3
sumit <sumit@Sumit.mshome.net>
2022-03-08 15:17:20 +0530

    Done by sumit and dhiraj

---
+++ Desktop/Adv Java Assignment eclipse/DateTime/src/main/java/fi/datetime/DateTime.java
@@ -0,0 +1,23 @@
+package fi.datetime;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/DateTime")
+public class DateTime extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("curent date : "+ new Date());
+	}
+
+}
