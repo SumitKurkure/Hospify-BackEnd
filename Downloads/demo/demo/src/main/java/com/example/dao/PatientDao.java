165438bee121b3204a1e8a555853be8865d89ff0
SumitKurkure <sumitkurkure@gmail.com>
2022-04-02 18:59:30 +0530

    By sumit new 1

--- Downloads/demo/demo/src/main/java/com/example/dao/PatientDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.pojo.Patient;
-
-public interface PatientDao extends JpaRepository<Patient, Integer>{
-	public void loginDetails();
-}
