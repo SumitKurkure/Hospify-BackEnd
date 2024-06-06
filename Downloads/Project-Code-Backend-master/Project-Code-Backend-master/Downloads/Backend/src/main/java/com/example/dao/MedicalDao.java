b5d75826aa78a789ae9f16277196e4f4dd572455
SumitKurkure <sumitkurkure@gmail.com>
2024-06-06 10:00:54 +0530

    unwanted folder removed

--- Downloads/Project-Code-Backend-master/Project-Code-Backend-master/Downloads/Backend/src/main/java/com/example/dao/MedicalDao.java
+++
@@ -1,9 +0,0 @@
-//package com.example.dao;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface MedicalDao extends JpaRepository<Medical, Integer> {
-//
-//}
--- Downloads/Project-Code-Backend-master/Project-Code-Backend-master/Downloads/Backend/src/main/java/com/example/service/TreatmentService.java
+++
@@ -1,9 +0,0 @@
-//package com.example.service;
-//
-//import java.util.List;
-//
-//public interface TreatmentService {
-//public String registerTreatment(Treatment treatment);
-//	
-//	public List<Treatment> getTreatment(Treatment treatment);
-//}
